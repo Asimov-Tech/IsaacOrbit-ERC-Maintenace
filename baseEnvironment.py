33e40a2599851fa79cbd41886cf75e55d76fe521
John-Dillermand <ctuk21@student.aau.dk>
2024-03-20 11:44:49 +0100

    it works?

---
+++ baseEnvironment.py
@@ -0,0 +1,118 @@
+# Copyright (c) 2022-2024, The ORBIT Project Developers.
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""
+This script demonstrates how to create a simple environment with a cartpole. It combines the concepts of
+scene, action, observation and randomization managers to create an environment.
+"""
+
+from __future__ import annotations
+
+"""Launch Isaac Sim Simulator first."""
+
+
+import argparse
+
+from omni.isaac.orbit.app import AppLauncher
+
+# add argparse arguments
+parser = argparse.ArgumentParser(description="Tutorial on creating a cartpole base environment.")
+parser.add_argument("--num_envs", type=int, default=64, help="Number of environments to spawn.")
+
+# append AppLauncher cli args
+AppLauncher.add_app_launcher_args(parser)
+# parse the arguments
+args_cli = parser.parse_args()
+
+# launch omniverse app
+app_launcher = AppLauncher(args_cli)
+simulation_app = app_launcher.app
+
+"""Rest everything follows."""
+#dwadwada testa
+#did it work
+import math
+import torch
+import omni.isaac.orbit.envs.mdp as mdp
+from omni.isaac.orbit.envs import BaseEnv, BaseEnvCfg
+from omni.isaac.orbit.managers import ObservationGroupCfg as ObsGroup
+from omni.isaac.orbit.managers import ObservationTermCfg as ObsTerm
+from omni.isaac.orbit.managers import RandomizationTermCfg as RandTerm
+from omni.isaac.orbit.managers import SceneEntityCfg
+from omni.isaac.orbit.utils import configclass
+#from omni.isaac.orbit_tasks.classic.cartpole.cartpole_env_cfg import CartpoleSceneCfg
+import base_taskboards_env_cfg 
+
+from omni.isaac.orbit_assets import UR10_CFG  # isort:skip
+
+###--------------------------------------------------------------------------------------------
+
+#@configclass
+#class ActionsCfg:
+#    """Action specifications for the environment."""
+#
+#    joint_efforts = mdp.JointEffortActionCfg(asset_name="robot", joint_names=[".*"], scale=5.0)
+
+
+
+@configclass
+class TaskBoardEnvCfg(BaseEnvCfg):
+    """Configuration for the Task board environment."""
+
+    # Scene settings
+    scene = base_taskboards_env_cfg.TaskBoardSceneCfg(num_envs=1024, env_spacing=2.5)
+    
+    # Basic settings
+    #observations = ObservationsCfg()
+    #actions = ActionsCfg()
+    #randomization = RandomizationCfg()
+
+    def __post_init__(self):
+        """Post initialization."""
+        # viewer settings
+        self.viewer.eye = [4.5, 0.0, 6.0]
+        self.viewer.lookat = [0.0, 0.0, 2.0]
+        # step settings
+        self.decimation = 4  # env step every 4 sim steps: 200Hz / 4 = 50Hz
+        # simulation settings
+        self.sim.dt = 0.005  # sim step every 5ms: 200Hz
+
+
+def main():
+    """Main function."""
+    # parse the arguments
+    env_cfg = TaskBoardEnvCfg()
+    env_cfg.scene.num_envs = args_cli.num_envs
+    # setup base environment
+    env = BaseEnv(cfg=env_cfg)
+
+    # simulate physics
+    count = 0
+    while simulation_app.is_running():
+        with torch.inference_mode():
+            # reset
+            if count % 300 == 0:
+                count = 0
+        #        env.reset()
+                print("-" * 80)
+                print("[INFO]: Resetting environment...")
+            # sample random actions
+            joint_efforts = torch.randn_like(env.action_manager.action)
+            # step the environment
+            obs, _ = env.step(joint_efforts)
+            # print current orientation of pole
+            #print("[Env 0]: Pole joint: ", obs["policy"][0][1].item())
+            # update counter
+            count += 1
+
+    # close the environment
+    env.close()
+
+
+if __name__ == "__main__":
+    # run the main function
+    main()
+    # close sim app
+    simulation_app.close()
